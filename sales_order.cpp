e044de8216ef8c2a4fce8447bb357fac760c0f78
OmerKhan360 <omer.khan2384@gmail.com>
2024-05-04 02:19:12 +0500

    by Omer

--- sales_order.cpp
+++ sales_order.cpp
@@ -7,25 +7,25 @@ class admin
 };
 class Shopping
 {
-      private:
-      int S_code;
-      float S_price;
-      float S_discount;
-      string S_name;
-      public:
-      void S_menu();
-      void S_admin();
-      void S_buyer();
-      void S_add();
-      void S_edit();
-      void S_remove();
-      void S_list();
-      void S_receipt();
+    private:
+        int S_code;
+        float S_price;
+        float S_discount;
+        string S_name;
+    public:
+        void S_menu();
+        void S_admin();
+        void S_buyer();
+        void S_add();
+        void S_edit();
+        void S_remove();
+        void S_list();
+        void S_receipt();
 
 };
 void Shopping :: S_menu()
 {
-      m;
+      m:
       int m_choice;
       string m_email;
       int m_password;
@@ -82,9 +82,71 @@ void Shopping :: S_menu()
 
 
 }
-class user
+void Shopping :: S_admin()
 {
-};
+    int choice;
+    cout<<"\n\n\n\t\t\t  Adminstrator Menu";
+    cout<<"\n\t\t\t|_____1) Add the Product________|";
+    cout<<"\n\t\t\t|                               |";
+    cout<<"\n\t\t\t|_____2) Delete the Product_____|";
+    cout<<"\n\t\t\t|                               |";
+    cout<<"\n\t\t\t|_____3) Modify the Product_____|";
+    cout<<"\n\t\t\t|                               |";
+    cout<<"\n\t\t\t|_____4) Return Main Menu_______|";
+    m:
+        cout<<"\n\n\t Please enter your choice ";
+        cin>>choice;
+
+        switch(choice)
+        {
+
+            case 1:
+                S_add();
+                break;
+            case 2:
+                S_remove();
+                break;
+            case 3:
+                S_edit();
+                break;
+            case 4:
+                S_menu();
+                break;
+            default:
+                cout<<"Please Select From the Given Option ";
+                goto m;
+        }
+}
+
+void Shopping :: S_buyer()
+{
+    int choice;
+    cout<<"\t\t\t  Buyer \n";
+    cout<<"\t\t\t__________________ \n";
+    cout<<"                         \n";
+    cout<<"\t\t\t1) Buy product     \n";
+    cout<<"                         \n";
+    cout<<"\t\t\t2)  GO Back        \n";
+
+    m:
+        cout<<"\n\n\t Please enter your choice ";
+        cin>>choice;
+
+        switch(choice)
+        {
+
+            case 1:
+                S_receipt();
+                break;
+
+            case 2:
+                S_menu();
+                break;
+            default:
+                cout<<"Please Select From the Given Option ";
+                goto m;
+        }
+}
 
 int main()
 {
